4cacdaa88e62663730e0d8f1b1f1c417b8886b3b
zhanglindeng <zhanglindeng@outlook.com>
2018-11-03 17:43:32 +0800

    primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { SelectItem, SelectItemGroup } from 'primeng/api';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeng-admin';
+  cities: City[];
+
+  selectedCity: City;
+
+  cars: SelectItem[];
+
+  selectedCar1: string;
+
+  selectedCar2 = 'BMW';
+
+  selectedCar3: string;
+
+  groupedCars: SelectItemGroup[];
+
+  constructor() {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+
+    this.cars = [
+      { label: 'Audi', value: 'Audi' },
+      { label: 'BMW', value: 'BMW' },
+      { label: 'Fiat', value: 'Fiat' },
+      { label: 'Ford', value: 'Ford' },
+      { label: 'Honda', value: 'Honda' },
+      { label: 'Jaguar', value: 'Jaguar' },
+      { label: 'Mercedes', value: 'Mercedes' },
+      { label: 'Renault', value: 'Renault' },
+      { label: 'VW', value: 'VW' },
+      { label: 'Volvo', value: 'Volvo' }
+    ];
+
+    this.groupedCars = [
+      {
+        label: 'Germany', value: 'germany.png',
+        items: [
+          { label: 'Audi', value: 'Audi' },
+          { label: 'BMW', value: 'BMW' },
+          { label: 'Mercedes', value: 'Mercedes' }
+        ]
+      },
+      {
+        label: 'USA', value: 'usa.png',
+        items: [
+          { label: 'Cadillac', value: 'Cadillac' },
+          { label: 'Ford', value: 'Ford' },
+          { label: 'GMC', value: 'GMC' }
+        ]
+      },
+      {
+        label: 'Japan', value: 'japan.png',
+        items: [
+          { label: 'Honda', value: 'Honda' },
+          { label: 'Mazda', value: 'Mazda' },
+          { label: 'Toyota', value: 'Toyota' }
+        ]
+      }
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    DropdownModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
